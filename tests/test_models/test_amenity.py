b454754ee07118cdde0cd2ff72362ea5af1891ff
mburudev <ianmburu999@gmail.com>
2023-09-22 02:28:02 -0700

    Task 1 amenity test

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -2,7 +2,7 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
-
+import pep8
 
 class test_Amenity(test_basemodel):
     """ """
@@ -17,3 +17,11 @@ class test_Amenity(test_basemodel):
         """ """
         new = self.value()
         self.assertEqual(type(new.name), str)
+class TestPEP8Conformance(unittest.TestCase):
+    """Test code style conformance using PEP8"""
+
+    def test_pep8_conformance(self):
+        """Test that the entire script conforms to PEP8."""
+        style = pep8.StyleGuide(quiet=True)
+        result = style.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0, "PEP8 style violations found.")
